81ee738409f497db5f62a4d40d45af36660a9624
Charles <charlesli41@gmail.com>
2022-07-14 14:30:48 -0400

    last commit on main

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const myApp = {};
 
 myApp.key = "ca2c13c7d22715aaa9867db7666b846d";
 myApp.url =
-    " https://api.themoviedb.org/3/discover/movie?api_key=ca2c13c7d22715aaa9867db7666b846d&language=en-US&sort_by=vote_average.desc&include_adult=false&include_video=false&release_date.gte=2000&vote_count.gte=3000&with_watch_monetization_types=flatrate";
+    " https://api.themoviedb.org/3/discover/movie?api_key=ca2c13c7d22715aaa9867db7666b846d&language=en-US&sort_by=vote_average.asc&include_adult=false&include_video=false&release_date.gte=2000&vote_count.gte=3000&with_watch_monetization_types=flatrate";
 myApp.init = () => {};
 
 const submitButton = document.querySelector(".submitButton");
